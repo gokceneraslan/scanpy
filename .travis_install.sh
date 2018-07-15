8ab14ce374c0997de19862a78f7dde43aeaeb58e
Gökcen Eraslan <gokcen.eraslan@gmail.com>
2018-07-15 14:25:48 +0200

    Change hdf5 lib check order

--- .travis_install.sh
+++ .travis_install.sh
@@ -25,10 +25,10 @@ popd
 conda create -n testenv --yes python=$TRAVIS_PYTHON_VERSION pip pytest hdf5 \
       numba scikit-learn statsmodels
 
-ldd /home/travis/miniconda3/envs/testenv/lib/python*/site-packages/h5py/h5.*so
 
 source activate testenv
 
 pip install -r requires.txt
+ldd /home/travis/miniconda3/envs/testenv/lib/python*/site-packages/h5py/h5.*so
 pip install docutils
 python setup.py develop
